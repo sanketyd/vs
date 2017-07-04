d55141266ab9697d4945e52d86763ee289307ea7
sanketyd <yadavsanket17@yahoo.in>
2017-07-04 20:59:39 +0530

    Updated contacts.php

--- vs/app/views/pages/contact_us.php
+++ vs/app/views/pages/contact_us.php
@@ -1,5 +1,7 @@
 <?php include (APP_ROOT.'/app/views/base.php'); ?>
-
+<?php startblock('head')?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<?php endblock()?>
 <?php startblock('central_pane') ?>
 
 
@@ -8,7 +10,7 @@
 	<br>
 
 	<p>
-	join us on facebook<br> 
+	join us on facebook<br>
 	facebook page <a href='https://www.facebook.com/vsiitk'>  https://www.facebook.com/vsiitk</a></br>
 	facebook group <a href='https://www.facebook.com/groups/vsiitk/'> https://www.facebook.com/groups/vsiitk/</a></br>
 	you tube <a href='http://www.youtube.com/user/vsiitk'>http://www.youtube.com/user/vsiitk</a></br>
@@ -16,13 +18,172 @@
 </p>
 
 <br>
-
+<h3>STUDENT REPRESENTATIVES</h3>
+<table class="table table-bordered" style="font-size: 120%;">
+	<tr>
+		<th>
+			Function
+		</th>
+		<th>
+			Name
+		</th>
+		<th>
+			Program
+		</th>
+	</tr>
+	<tr>
+		<td>
+			Student Advisor
+		</td>
+		<td>
+			Shubhadip Mitra
+		</td>
+		<td>
+			Phd; CSE<br />
+			9935600145<br />
+			<a href="mailto:smitr@iitk.ac.in">smitr@iitk.ac.in</a>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Head
+		</td>
+		<td>
+			Arjak Bhattacharjee
+		</td>
+		<td>
+			M.Tech; MSE<br />
+			7318019245<br />
+			<a href="mailto:arjak@iitk.ac.in">arjak@iitk.ac.in</a>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Head
+		</td>
+		<td>
+			Saksham Sharma
+		</td>
+		<td>
+			MS; Maths<br />
+			9045166728<br />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Head
+		</td>
+		<td>
+			Nazeen Mansoori
+		</td>
+		<td>
+			BS; Economics<br />
+			7270038629<br />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Head
+		</td>
+		<td>
+			Bijai
+		</td>
+		<td>
+			B.Tech; CE<br />
+			<br />
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Meditation in charge
+		</td>
+		<td>
+			Koustuv Ray
+		</td>
+		<td>
+			Phd; CHE<br />
+			8090829640<br />
+			<a href="mailto:koustuvr@iitk.ac.in">koustuvr@iitk.ac.in</a>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Course incharge
+		</td>
+		<td>
+			Anandogopal Chatterjee
+		</td>
+		<td>
+			Phd; Physics<br />
+			8574422684
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Public relations
+		</td>
+		<td>
+			Sanghamitra
+		</td>
+		<td>
+			Phd; Physics<br />
+			9161493084
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Media and web
+		</td>
+		<td>
+			Arjak Bhattacharjee<br />
+			Bhanu Mahto
+		</td>
+		<td>
+			7318019245<br />
+			8986229614
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Videographer
+		</td>
+		<td>
+			Badal pradhan
+		</td>
+		<td>
+			9005751991
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Video editing
+		</td>
+		<td>
+			T.Das
+		</td>
+		<td>
+			9839457863
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Cultural Activities & Health camp incharge
+		</td>
+		<td>
+			Nazeen Mansoori<br />
+			Bijai
+		</td>
+		<td>
+			7270038629
+		</td>
+	</tr>
+</table>
 <table id='coordinators_list' border='1' style="font-size:14px" cellpadding='5' align='center'>
 	<tr>
 		<td colspan='3'>
 			<b style="color:brown">Faculty advisorsory commitee</b>:
 			<ul>
-			
+
 			<li><a href="http://www.iitk.ac.in/ime/jayanta/OLD/" target='_blank'>Prof. Jayanta Chatterjee</a>, IIT Kanpur.</li>
 				<li><a href="http://www.iitk.ac.in/aero/dpm/index.htm" target='_blank'>Prof. D. P. Mishra</a>, IIT Kanpur.</li>
 				<li><a href="http://www.iitk.ac.in/che/rp.htm" target='_blank'>Prof. Raj Ganesh S Pala</a>, IIT Kanpur.</li>
@@ -38,13 +199,13 @@
 			</ul>
 		</td>
 	</tr>
-	
-	<tr>
+
+	<!--<tr>
 		<td colspan='3' style='text-align:center'>
 			<b style="color:brown">Chief student co-ordinators</b>
 		</td>
 	</tr>
-	
+
 	<tr>
 	         <td>
 			<b>Name</b>: Nakul Surana<br>
@@ -52,14 +213,14 @@
 			<b>Mobile</b>: 8604295936<br>
 			<b>Dept</b>: MTH
 		</td>
-                
+
                 <td>
 			<b>Name</b>:Chandan K Talele<br>
 			<b>e-mail</b>: <a href="mailto:chandant@iitk.ac.in">chandant@iitk.ac.in</a><br>
 			<b>Mobile</b>: 9451705838<br>
-			<b>Dept</b>: 
+			<b>Dept</b>:
 		</td>
- 
+
                 <td>
 			<b>Name</b>: Sakshi Choukse<br>
 			<b>e-mail</b>: <a href="mailto:sakshic@iitk.ac.in">sakshic@iitk.ac.in</a><br>
@@ -87,7 +248,7 @@
 			<b>Mobile</b>: 9935600145<br>
 			<b>Dept</b>: CSE
 		</td>
-		
+
 	     </tr>
              <tr>
 		<td>
@@ -97,18 +258,13 @@
 			<b>Dept</b>: CSE
 		</td>
 
-		
+
 	</tr>
-	
-	<tr>
-		<td colspan='3' style='text-align:center'>
-			<b style="color:brown">Core team members</b>
-		</td>
-	</tr>
-	
+
+
 
 	<tr>
-	<!--	<td>
+		<td>
 			<b>Name</b>: Dr. Prashant Jha<br>
 			<b>e-mail</b>: <a href="mailto:drpjha@iitk.ac.in">drpjha@iitk.ac.in</a><br>
 			<b>Mobile</b>: 9450600807<br>
@@ -127,95 +283,95 @@
 			<b>Dept</b>: CSE
 		</td>
 	</tr>
-	-->
+-->
+<tr>
+	<td colspan='3' style='text-align:center'>
+		<b style="color:brown">Core team members</b>
+	</td>
+</tr>
 	<tr>
 		<td>
-			<b>Name</b>: Abhinav Tripathi<br>
-			<b>e-mail</b>: <a href="mailto:abhinavt@iitk.ac.in">abhinavt@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9161493084<br>
-			<b>Dept</b>: CSE
+			<b>Name</b>: Sandhya Rai<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: 7783977439<br>
+			<b>Dept</b>: CHE
 		</td>
 		<td>
-			<b>Name</b>: Joyeeta Mandal<br>
-			<b>e-mail</b>: <a href="mailto:joyeetam@iitk.ac.in">joyeetam@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9452522363<br>
-			<b>Dept</b>: CSE
+			<b>Name</b>: D.P. Shukla<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: 9450588596<br>
+			<b>Dept</b>: EE
 		</td>
 		<td>
-			<b>Name</b>: Deepak Ojha<br>
-			<b>e-mail</b>: <a href="mailto:dojha@iitk.ac.in">dojha@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9005205318<br>
-			<b>Dept</b>: CHM
+			<b>Name</b>: Avinash<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: <br>
+			<b>Dept</b>: EE
 		</td>
 	</tr>
 
 	<tr>
 		<td>
-			<b>Name</b>: Satyashekhar<br>
+			<b>Name</b>: Nikhil Bansal<br>
 			<b>e-mail</b>: <a href="mailto:"></a><br>
 			<b>Mobile</b>: <br>
-			<b>Dept</b>: 
+			<b>Dept</b>: MSE
 		</td>
 		<td>
-			<b>Name</b>: Gopinath<br>
+			<b>Name</b>: Saumita Ganguly<br>
 		<b>e-mail</b>: <a href="mailto:"></a><br>
 			<b>Mobile</b>:<br>
-			<b>Dept</b>:
+			<b>Dept</b>: MSE
 		</td>
 		<td>
-			<b>Name</b>: Asit Naik<br>
-			<b>e-mail</b>: <a href="mailto:asitnaik@iitk.ac.in">asitnaik@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9621452179<br>
-			<b>Dept</b>: CSE
+			<b>Name</b>: Rita Maurya<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: <br>
+			<b>Dept</b>: MSE
 		</td>
 	</tr>
-	
+
 	<tr>
 		<td>
-			<b>Name</b>: Salman Mohammad<br>
-			<b>e-mail</b>: <a href="mailto:salmanmd@iitk.ac.in">salmanmd@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9005666084<br>
-			<b>Dept</b>: CSE
+			<b>Name</b>: Mizanur Rahaman<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: <br>
+			<b>Dept</b>: MSE
 		</td>
 		<td>
-			<b>Name</b>: Rahul Rao<br>
-			<b>e-mail</b>: <a href="mailto:raorahul@iitk.ac.in">raorahul@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9559442989<br>
-			<b>Dept</b>: EE
+			<b>Name</b>: Priya Kusharam<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: <br>
+			<b>Dept</b>: MSE
 		</td>
 		<td>
-			<b>Name</b>: Siddharth Sharma<br>
-			<b>e-mail</b>: <a href="mailto:sids@iitk.ac.in">sids@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9005664260<br>
-			<b>Dept</b>: CHE
+			<b>Name</b>: Satyashekhar Chakka<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: <br>
+			<b>Dept</b>:
 		</td>
 	</tr>
 
 
 <tr>
 	<td>
-			<b>Name</b>: Aditya Singh<br>
-			<b>e-mail</b>: <a href="mailto:adisingh@iitk.ac.in">adisingh@iitk.ac.in</a><br>
-			<b>Mobile</b>: 7275131149<br>
-			<b>Dept</b>: CHE
+			<b>Name</b>:GR Gopi<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: <br>
+			<b>Dept</b>:
 		</td>
 	<td>
-			<b>Name</b>: Priyankar Talukdar<br>
-			<b>e-mail</b>: <a href="mailto:talukdar@iitk.ac.in">talukdar@iitk.ac.in</a><br>
-			<b>Mobile</b>: 7275966774<br>
-			<b>Dept</b>: Eco
+			<b>Name</b>: Rohan<br>
+			<b>e-mail</b>: <a href="mailto:"></a><br>
+			<b>Mobile</b>: <br>
+			<b>Dept</b>:
 		</td>
 
-		<td>
-			<b>Name</b>: Naveen Kumar Verma<br>
-			<b>e-mail</b>: <a href="mailto:nkrverma@iitk.ac.in">nkrverma@iitk.ac.in</a><br>
-			<b>Mobile</b>: 9559553520<br>
-			<b>Dept</b>: 
-		</td>
+
 	</tr>
 
 
-		<tr>
+		<!--<tr>
 	<td>
 			<b>Name</b>: sahil solanki<br>
 			<b>e-mail</b>: <a href="mailto:ssolanki@iitk.ac.in">ssolanki@iitk.ac.in</a><br>
@@ -233,7 +389,7 @@
 			<b>Name</b>: dharmapal singh<br>
 			<b>e-mail</b>: <a href="mailto:dbangar@iitk.ac.in">dbangar@iitk.ac.in</a><br>
 			<b>Mobile</b>: 8953431590<br>
-			<b>Dept</b>: 
+			<b>Dept</b>:
 		</td>
 	</tr>
 
@@ -257,6 +413,6 @@
 			<b>Mobile</b>: 9795337246<br>
 			<b>Dept</b>: CE
 		</td>
-	</tr>
+	</tr>-->
 </table>
 <?php endblock() ?>
